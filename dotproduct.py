4134742e8881543f6684df122a91526c74aa6aa4
Colin <colin.glesner@gmail.com>
2019-06-23 22:16:21 -0600

    Created initial function and created unit tests.

--- dotproduct.py
+++ dotproduct.py
@@ -1,10 +1,20 @@
 """module for calculating dot product or inner product of two vectors."""
 
+
 def calculate(a, b):
-    """calculate the dot product of two vectors."""
+    """Calculate the dot product of two vectors of the same length."""
+    if len(b) != len(a):
+        return None
 
     product = 0
     for a_i, b_i in zip(a, b):
-        product += a_i*b_i
+        try:
+            product += a_i*b_i
+        except TypeError:
+            return None
 
     return product
+
+def orthogonal(a):
+    """Return an arbitrary vector orthogonal to the input vector."""
+    return None
--- test_dotproduct.py
+++ test_dotproduct.py
@@ -5,36 +5,35 @@ import unittest
 import dotproduct
 
 
-class TestDotProduct(unittest.TestCase):
-    """Test the dotproduct module."""
+class TestCalculate(unittest.TestCase):
+    """Test the calculate function."""
 
-    def test_dotproduct_null(self):
+    def test_calculate_null(self):
         """Test that the dot product of two null vectors is zero. Expects Tuples, returns float."""
         a = (0, 0)
         b = (0, 0)
         self.assertEqual(0, dotproduct.calculate(a, b))
 
-
-    def test_dotproduct_orthogonal(self):
+    def test_calculate_orthogonal(self):
         """Test that orthogonal vectors result in a zero product."""
         a = (1, 0)
         b = (0, 1)
         self.assertEqual(0, dotproduct.calculate(a, b))
 
-    def test_dotproduct_parallel(self):
+    def test_calculate_parallel(self):
         """Test that parallel vectors result in the sum of squares of the vector's components."""
         a = (2, 3, 5)
         a2 = 2*2 + 3*3 + 5*5
         self.assertEqual(a2, dotproduct.calculate(a, a))
 
-    def test_dotproduct_antiparallel(self):
+    def test_calculate_antiparallel(self):
         """Test that anti-parallel vectors result in the negative of the sum of the vector's components."""
         a = (2, 3, 5)
         a_neg = (-2, -3, -5)
         a_a_neg = -1*(2*2 + 3*3 + 5*5)
         self.assertEqual(a_a_neg, dotproduct.calculate(a, a_neg))
 
-    def test_dotproduct_examples(self):
+    def test_calculate_examples(self):
         """Test arbitrary examples of dot products."""
         a = (2, 3, 5)
 
@@ -54,11 +53,38 @@ class TestDotProduct(unittest.TestCase):
         a_e = 31
         self.assertEqual(a_e, dotproduct.calculate(a, e))
 
-    def test_dotproduct_comutivity(self):
+    def test_calculate_commutivity(self):
         """The dot product is commutable. a dot b = b dot a"""
         a = (2, 3, 5, 7)
         b = (1, 1, 2, 3)
         self.assertEqual(dotproduct.calculate(a, b), dotproduct.calculate(b, a))
+
+    def test_calculate_vector_missmatch(self):
+        """Test that the method returns None if vectors of different length are passed in."""
+        a = (2, 3, 5)
+        b = (7, 11, 13, 17)
+        self.assertEqual(dotproduct.calculate(a, b), None)
+
+    def test_calculate_invalid_vector(self):
+        """Test that the method returns None if vectors contain invalid elements."""
+        a = (2, 3, 5)
+        b = (7, 11, 'a')
+        self.assertEqual(dotproduct.calculate(a, b), None)
+
+
+class TestOrthogonal(unittest.TestCase):
+    """Test the orthogonal function."""
+
+    def test_orthogonal_1D(self):
+        """Test that function returns None if 1-D Vector passed in."""
+        a = (2,)
+        self.assertEqual(dotproduct.orthogonal(a), None)
+
+    def test_orthogonal_2D(self):
+        """Test that the function returns an orthogonal vector if a 2-D Vector is passed in."""
+        a = (1, 1)
+        a_orth = dotproduct.orthogonal(a)
+        self.assertEqual(dotproduct.calculate(a, a_orth), 0)
 
 if __name__ == "__main__":
     unittest.main()
